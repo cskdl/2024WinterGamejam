c8355db8859c5866dfb0c13023e24df8a4cb91df
cksdl <cksdl2923@gmail.com>
2024-01-17 13:55:11 +0900

    UpdateEnemy

--- 2024_Winter_Gamejam/Assets/Scripts/Seram/Scripts/Enemy.cs
+++ 2024_Winter_Gamejam/Assets/Scripts/Seram/Scripts/Enemy.cs
@@ -33,32 +33,11 @@ public class Enemy : MonoBehaviour
         target = GameObject.FindObjectOfType<PlayerAttack>().GetComponent<Rigidbody2D>();
     }
 
-    //FixedUpdate가 Update보다 더 자주 실행되고 있어 주석처리 해둔 상태
-    //void FixedUpdate()
-    //{
-    //    if (m_timer > 0)
-    //    {
-    //        m_timer -= Time.fixedDeltaTime;
-    //    }
-    //    else
-    //    {
-    //        if (m_delay <= 0)
-    //        {
-    //            MoveTowardsTarget();
-    //            LookAtTarget();
-    //        }
-    //        else
-    //        {
-    //            m_delay -= Time.fixedDeltaTime;
-    //        }
-    //    }
-    //}
-
-    private void Update()
+    void FixedUpdate()
     {
         if (m_timer > 0)
         {
-            m_timer -= Time.deltaTime;
+            m_timer -= Time.fixedDeltaTime;
         }
         else
         {
@@ -69,32 +48,46 @@ public class Enemy : MonoBehaviour
             }
             else
             {
-                MoveBump();
-                m_delay -= Time.deltaTime;
+                m_delay -= Time.fixedDeltaTime;
             }
         }
     }
 
+    //private void Update()
+    //{
+    //    if (m_timer > 0)
+    //    {
+    //        m_timer -= Time.deltaTime;
+    //    }
+    //    else
+    //    {
+    //        if (m_delay <= 0)
+    //        {
+    //            MoveTowardsTarget();
+    //            LookAtTarget();
+    //        }
+    //        else
+    //        {
+    //            MoveBump();
+    //            m_delay -= Time.deltaTime;
+    //        }
+    //    }
+    //}
+
     void MoveTowardsTarget()
     {
-        //Vector2 temp = new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y);
         Vector2 temp = new Vector2(target.position.x, target.position.y);
         Vector2 dirVec = temp - rigid.position;
         Vector2 nextVec = dirVec.normalized;
-        //rigid.MovePosition((rigid.position + nextVec) * speed * Time.deltaTime);
-        //rigid.AddForce((nextVec - rigid.position.normalized) * speed);
-        //rigid.velocity = Vector3.zero;
-        rigid.velocity = this.transform.right * speed * Time.deltaTime;
+        rigid.velocity = this.transform.right * speed * Time.fixedDeltaTime;
     }
 
     void LookAtTarget()
     {
-        //Vector3 temp = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y);
         Vector3 temp = target.position;
         Vector3 targetDir = temp - transform.position;
         float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     void MoveBump()
