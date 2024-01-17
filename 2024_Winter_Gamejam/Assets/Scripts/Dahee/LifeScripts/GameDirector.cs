53e5bc1a767ee6a75f62ee44ab5e83e0a9b1dd8f
gaeZZang <128779897+gaeZZang@users.noreply.github.com>
2024-01-17 15:15:48 +0900

    LifeSetting

---
+++ 2024_Winter_Gamejam/Assets/Scripts/Dahee/LifeScripts/GameDirector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+
+    public GameObject[] lifes;
+
+    public void UpdateLives(int playerLife)
+    {
+        for (int i = 0; i < lifes.Length; i++)
+            this.lifes[i].SetActive(i < playerLife);
+    }
+
+
+    public void Init(int playerLife)
+    {
+        for (int i = 0; i < lifes.Length; i++)
+            this.lifes[i].SetActive(false);
+
+        for (int i = 0; i < playerLife; i++)
+            this.lifes[i].SetActive(true);
+    }
+  
+}
